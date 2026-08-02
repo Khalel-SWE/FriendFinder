b3860a789f92081e4d60c4ec7875ab913e17b778
Khalel-SWE <khalel.swe96@gmail.com>
2026-08-03 01:40:11 +0200

    Change ActivityController request mapping path
    Update ActivityController request mapping from "/api/activities" to "/friend-finder/activities" to align the controller route with the application's new base path. File changed: Back-End/friend-finder/src/main/java/com/media/friend_finder/controller/ActivityController.java.

--- Back-End/friend-finder/src/main/java/com/media/friend_finder/controller/ActivityController.java
+++ Back-End/friend-finder/src/main/java/com/media/friend_finder/controller/ActivityController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/activities")
+@RequestMapping("/friend-finder/activities")
 @RequiredArgsConstructor
 public class ActivityController {
 
