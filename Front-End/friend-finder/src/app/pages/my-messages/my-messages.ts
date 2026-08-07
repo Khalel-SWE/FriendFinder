0f2c7d810d0f0305a33f0ab3009d5737a23ddb8b
Khalel-SWE <khalel.swe96@gmail.com>
2026-08-07 23:35:27 +0200

    Redesign contact page with dark theme
    Refactored the contact page to use a dark color scheme with gold accents. Removed custom CSS styles in favor of Tailwind utilities, updated the form layout and styling, and improved visual hierarchy. Also added a skeleton my-messages component.

---
+++ Front-End/friend-finder/src/app/pages/my-messages/my-messages.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-messages',
+  imports: [],
+  templateUrl: './my-messages.html',
+  styleUrl: './my-messages.css',
+})
+export class MyMessages {}
