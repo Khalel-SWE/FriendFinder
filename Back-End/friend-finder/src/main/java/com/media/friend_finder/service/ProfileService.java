a3cabe7706ab5aefa8e1e94dc30b85b5bd67198e
Khalel-SWE <khalel.swe96@gmail.com>
2026-08-22 22:45:12 +0200

    Remove debug println from ProfileService
    Remove a leftover commented-out System.out.println(email) from ProfileService.getMyProfile to clean up transient debugging code. No functional behavior changes.

--- Back-End/friend-finder/src/main/java/com/media/friend_finder/service/ProfileService.java
+++ Back-End/friend-finder/src/main/java/com/media/friend_finder/service/ProfileService.java
@@ -24,8 +24,6 @@ public class ProfileService {
 
     public ProfileResponse getMyProfile(String email) {
 
-//        System.out.println(email);
-
         Profile p = profileRepository.findByUserEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Profile not found"));
 
